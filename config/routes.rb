f2e9ea6e4af91e5635dd64f943de6f88cccc617d
you040385 <you040385@gmail.com>
2022-03-13 16:10:27 +0900

    ログインのルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'sessions#new'
-  resources :sessions, only: %i[create]
+  post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :photos, only: %i[index new create]
 end
