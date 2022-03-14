4b78a991165a77a12517b0188cc96309fd9f028d
you040385 <you040385@gmail.com>
2022-03-14 20:00:25 +0900

    Tweet -> MyTweetApp::Tweet

--- app/controllers/photos/tweets_controller.rb
+++ app/controllers/photos/tweets_controller.rb
@@ -6,7 +6,7 @@ module Photos
     before_action :authenticate
 
     def create
-      tweet = Tweet.new(token: my_tweet_app_token, title: @photo.title, url: photo_file_url(@photo))
+      tweet = MyTweetApp::Tweet.new(token: my_tweet_app_token, title: @photo.title, url: photo_file_url(@photo))
 
       respond_to do |format|
         if tweet.save
